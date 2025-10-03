716f516e32ef0fceee98e2e8964d582f41b2d10c
Kieran Pritchard <kierock03@gmail.com>
2025-10-03 12:06:50 +0100

    Add custom markers

--- google_maps_api/src/js/mapUtils.js
+++ google_maps_api/src/js/mapUtils.js
@@ -1,11 +1,14 @@
 /**
  * Loads the Google Maps API script.
+ * Once loaded, it initializes the map.
+ * @param {string} apiKey 
+ * @param {function} callback 
  * @returns {void}
  */
-function loadMapsAPI(apiKey) {
+function loadMapsAPI(apiKey, callback) {
 
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
     script.async = true;
     script.defer = true;
 
@@ -18,10 +21,39 @@ function loadMapsAPI(apiKey) {
 
 function initMap() {
     const mapOptions = {
-        center: new google.maps.LatLng(37.7749, -122.4194), // San Francisco
-        zoom: 7,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    customMarker = {
+        url: './assets/marker.png',
+        scaledSize: new google.maps.Size(50, 50) // Scale the icon
+    }
+
+    const markers = [
+        { position: { lat: 37.7749, lng: -122.4194 }, title: "San Francisco", options: { icon: customMarker } },
+        { position: { lat: 34.0522, lng: -118.2437 }, title: "Los Angeles", options: { icon: customMarker } },
+        { position: { lat: 36.1699, lng: -115.1398 }, title: "Las Vegas", options: { icon: customMarker } }
+    ];
+
+    markers.forEach(marker => {
+        addMarker(map, marker.position, marker.title, marker.options);
+    });
+
+    // Recalculate center based on markers
+    const bounds = new google.maps.LatLngBounds();
+    markers.forEach(marker => {
+        bounds.extend(marker.position);
+    });
+    map.fitBounds(bounds, padding = 50);
+}
+
+function addMarker(map, position, title, options = {}) {
+    const marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: title,
+        ...options
+    });
 }
